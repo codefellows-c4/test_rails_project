cf3b109b8675758c3990bd75e180af9cffc017b8
Raymond Lucke <ray@vasaristudio.com>
2013-10-24 15:23:37 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CfWelcome::Application.config.secret_key_base = '19ba29e437b765c123b11656d205e3e514ad89b8c72bbc108683e50f25c536e8cf964031586b619283684c12977d2015f61a6d6924d843de36ef97e2ec6d978b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CfWelcome::Application.config.session_store :cookie_store, key: '_cf_welcome_session'
